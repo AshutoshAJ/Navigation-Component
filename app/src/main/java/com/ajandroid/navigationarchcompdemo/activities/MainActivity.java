0f5dd00aec82618171c6dc52a3a66a7058fa94e9
AshutoshAJ <acool3103@gmail.com>
2021-09-15 21:06:58 +0530

    Added simple navigation button

--- app/src/main/java/com/ajandroid/navigationarchcompdemo/activities/MainActivity.java
+++ app/src/main/java/com/ajandroid/navigationarchcompdemo/activities/MainActivity.java
@@ -2,15 +2,44 @@ package com.ajandroid.navigationarchcompdemo.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.ajandroid.navigationarchcompdemo.R;
+import com.ajandroid.navigationarchcompdemo.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+
+        setContentView(binding.getRoot());
+
+        initListeners();
+    }
+
+    private void initListeners() {
+
+        binding.simpleNavigationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SimpleNavigationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        binding.bottomNavigationBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/ajandroid/navigationarchcompdemo/activities/SimpleNavigationActivity.java
@@ -0,0 +1,16 @@
+package com.ajandroid.navigationarchcompdemo.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ajandroid.navigationarchcompdemo.R;
+
+public class SimpleNavigationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_simple_navigation);
+    }
+}
